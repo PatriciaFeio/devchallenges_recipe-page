4d1be0cf29d9ff97b46a4d3fca5759ba56caf90f
Patrícia Feio <patriciafeio@gmail.com>
2021-04-22 02:24:53 +0100

    update config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,42 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+        customForms: theme => ({
+      default: {
+        input: {
+          borderRadius: theme('borderRadius.lg')
+        }
+      }
+    }),
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat, sans-serif",
+        playfair: "Playfair Display, serif"
+      },
+      fontSize: {
+        xxxs: "0.5rem",
+        xxs: "0.625rem",
+        x24: "1.5rem"
+      },
+      textColor: {
+        "primary": "#333333",
+        "secondary": "#bdbdbd",
+        "secondaryFooter": "#828282",
+        "orange": "#F2994A"
+      },
+      backgroundColor: {
+        orange: "#F2994A"
+      }
+    }
   },
   variants: {
-    extend: {},
+    extend: {
+      textDecoration: ['checked'],
+      textDecoration: ['hover'],
+    },
   },
   plugins: [
-    require('@tailwindcss/typography')
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
   ],
 }
